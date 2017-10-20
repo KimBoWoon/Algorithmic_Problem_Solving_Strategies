51a9cc608391c423f99cd9e137cb4cf3d3137cc6
KimBoWoon <haioer@naver.com>
2017-10-20 22:39:40 +0900

    Triangle

---
+++ Algorithmic_Problem_Solving_Strategies/Trianglepath.cpp
@@ -0,0 +1,80 @@
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+#pragma warning(disable: 4996)
+
+#define MAX_NUMBER 100
+
+int testCase, n, maxSum;
+int triangle[101][101], cache[101][101], cache2[101][101][MAX_NUMBER * 100 + 1];
+
+// MAX_NUMBER : 한 칸에 들어갈 숫자의 최대치
+// (y, x) 위치까지 내려오기 전에 만난 숫자들의 합이 sum 일 때
+// 맨 아래줄까지 내려가면서 얻을 수 있는 최대 경로를 반환한다.
+int path1(int y, int x, int sum) {
+	// 기저 사례 : 맨 아래 줄까지 도달했을 경우
+	if (y == n - 1) {
+		return sum + triangle[y][x];
+	}
+	// 메모이제이션
+	int& ret = cache2[y][x][sum];
+	if (ret != -1) {
+		return ret;
+	}
+	sum += triangle[y][x];
+	return ret = max(path1(y + 1, x + 1, sum), path1(y + 1, x, sum));
+}
+
+// (y, x) 위치부터 맨 아래줄까지 내려가면서 얻을 수 있는 최대 경로의 합을 반환한다.
+int path2(int y, int x) {
+	// 기저 사례
+	if (y == n - 1) {
+		return triangle[y][x];
+	}
+	// 메모이제이션
+	int& ret = cache[y][x];
+	if (ret != -1) {
+		return ret;
+	}
+	return ret = max(path2(y + 1, x), path2(y + 1, x + 1)) + triangle[y][x];
+}
+
+int main() {
+	freopen("input.txt", "r", stdin);
+
+	scanf("%d", &testCase);
+
+	while (testCase--) {
+		scanf("%d", &n);
+
+		memset(cache, -1, sizeof(cache));
+		memset(cache2, -1, sizeof(cache2));
+		memset(triangle, 0, sizeof(triangle));
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < i + 1; j++) {
+				scanf("%d", &triangle[i][j]);
+			}
+		}
+
+		//printf("%d\n", path1(0, 0, 0));
+		printf("%d\n", path2(0, 0));
+
+		////cache[0][0] = max(triangle[0][0] + triangle[1][0], triangle[0][0] + triangle[1][1]);
+		//cache[0][0] = triangle[0][0];
+		//for (int i = 1; i < n; i++) {
+		//	for (int j = 0; j < i + 1; j++) {
+		//		cache[i][j] = max(cache[i - 1][j] + triangle[i][j], cache[i - 1][j] + triangle[i][j + 1]);
+		//	}
+		//}
+
+		//for (int i = 0; i < n; i++) {
+		//	if (cache[n - 1][i] > maxSum) {
+		//		maxSum = cache[n - 1][i];
+		//	}
+		//}
+		//printf("%d\n", maxSum);
+	}
+}
---
+++ Algorithmic_Problem_Solving_Strategies/Wildcard.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+
+// -1은 아직 답이 계산되지 않았음을 의미한다.
+// 1은 해당 입력들이 서로 대응됨을 의미한다.
+// 0은 해당 입력들이 서로 대응되지 않음을 의미한다.
+int cache[101][101];
+// 패턴과 문자열
+string W, S;
+vector<string> v;
+
+int testCase, n;
+
+// 와일드카드 패턴 w가 문자열 s에 대응되는지 여부를 반환한다.
+bool match(const string& w, const string& s) {
+	// w[pos]와 s[pos]를 맞춰 나간다.
+	int pos = 0;
+	while (pos < s.size() && pos < w.size() && (w[pos] == '?' || w[pos] == s[pos])) {
+		++pos;
+		// 더이상 대응할 수 없으면 왜 while문이 끝났는지 확인한다.
+		// 2. 패턴 끝에 도달해서 끝난 경우 : 문자열도 끝났어야 대응됨
+		if (pos == w.size()) {
+			return pos == s.size();
+		}
+		// 4. *를 만나서 끝난 경우 : *에 몇 글자를 대응해야 할지 재귀 호출하면서 확인한다.
+		if (w[pos] == '*') {
+			for (int skip = 0; pos + skip <= s.size(); ++skip) {
+				if (match(w.substr(pos + 1), s.substr(pos + skip))) {
+					return true;
+				}
+			}
+		}
+	}
+	// 이 외의 경우에는 모두 대응되지 않는다.
+	return false;
+}
+
+//// 와일드카드 패턴 W[w..]가 문자열 S[s..]에 대응되는지 여부를 반환한다.
+//bool matchMemoized(int w, int s) {
+//	// 메모이제이션
+//	int& ret = cache[w][s];
+//	if (ret != -1) {
+//		return ret;
+//	}
+//	// W[w]와 S[s]를 맞춰 나간다.
+//	while (s < S.size() && w < W.size() && (W[w] == '?' || W[w] == S[s])) {
+//		++w;
+//		++s;
+//	}
+//	// 더이상 대응할 수 없으면 왜 while문이 끝났는지 확인한다.
+//	// 2. 패턴 끝에 도달해서 끝난 경우 : 문자열도 끝났어야 함
+//	if (w == W.size()) {
+//		return ret = (s == S.size());
+//	}
+//	// 4. *를 만나서 끝난 경우 : *에 몇 글자를 대응해야 할지 재귀 호출하면서 확인한다.
+//	if (W[w] == '*') {
+//		for (int skip = 0; skip + s <= S.size(); ++skip) {
+//			if (matchMemoized(w + 1, s + skip)) {
+//				return ret = 1;
+//			}
+//		}
+//	}
+//	// 3. 이외의 경우에는 모두 대응되지 않는다.
+//	return ret = 0;
+//}
+
+// 와일드카드 패턴 W[w..]가 문자열 S[s..]에 대응되는지 여부를 반환한다.
+bool matchMemoized(int w, int s) {
+	// 메모이제이션
+	int& ret = cache[w][s];
+	if (ret != -1) {
+		return ret;
+	}
+	// W[w]와 S[s]를 맞춰 나간다.
+	if (s < S.size() && w < W.size() && (W[w] == '?' || W[w] == S[s])) {
+		return ret = matchMemoized(w + 1, s + 1);
+	}
+	// 더이상 대응할 수 없으면 왜 while문이 끝났는지 확인한다.
+	// 2. 패턴 끝에 도달해서 끝난 경우 : 문자열도 끝났어야 함
+	if (w == W.size()) {
+		return ret = (s == S.size());
+	}
+	// 4. *를 만나서 끝난 경우 : *에 몇 글자를 대응해야 할지 재귀 호출하면서 확인한다.
+	if (W[w] == '*') {
+		if (matchMemoized(w + 1, s) || (s < S.size() && matchMemoized(w, s + 1))) {
+			return ret = 1;
+		}
+	}
+	// 3. 이외의 경우에는 모두 대응되지 않는다.
+	return ret = 0;
+}
+
+int main() {
+	ifstream cin("input.txt");
+
+	cin >> testCase;
+
+	while (testCase--) {
+		cin >> W >> n;
+
+		for (int i = 0; i < n; i++) {
+			cin >> S;
+
+			memset(cache, -1, sizeof(cache));
+
+			/*if (match(W, S)) {
+				v.push_back(S);
+			}*/
+			if (matchMemoized(0, 0)) {
+				v.push_back(S);
+			}
+		}
+		sort(v.begin(), v.end());
+		for (int i = 0; i < v.size(); i++) {
+			cout << v[i] << endl;
+		}
+		v.clear();
+	}
+}
